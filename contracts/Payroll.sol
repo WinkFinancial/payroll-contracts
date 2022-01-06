4a0dc54aa7fad0be829d4f50c6982b0993be4ea1
Cryptoiner <daniel@thinkanddev.com>
2022-01-05 20:16:31 -0600

    Access Roles and fixed typos

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -3,14 +3,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../interfaces/IERC20.sol";
 import "hardhat/console.sol";
-import "./interfaces/ERC20Basic.sol";
 
 /**
  * @title A contract that allows multiple payments in one transaction
  * @author Lucas Marc
  */
-contract Payroll is Initializable {
+contract Payroll is Initializable, AccessControl {
+
+    bytes32 public constant PAYER_ROLE = keccak256("PAYER_ROLE");
+    bytes32 public constant ADMIN_ROLE = 0x00;
 
     modifier onlyOwner {
         require(
@@ -25,42 +29,41 @@ contract Payroll is Initializable {
     function initialize(address _owner) public initializer {
         console.log("Deploying a Payroll with owner: ", _owner);
         owner = _owner;
+        _setupRole(ADMIN_ROLE, _owner);
+        _setupRole(PAYER_ROLE, _owner);
     }
 
-    event BatchPaymentFinished(address[] _receivers, 
-                            uint256[] _ammountsToTransfer);
- 
+    event BatchPaymentFinished(address[] _receivers,
+        uint256[] _amountsToTransfer);
+
     /**
      * Performs the payment to the given addresses
      * @param _erc20TokenAddress The address of the ERC20 token to transfer
      * @param _receivers The array of payment receivers
-     * @param _ammountsToTransfer The array of payments' amounts to perform. The amount will be transfered to the address on _receivers with the same index.
+     * @param _amountsToTransfer The array of payments' amounts to perform. The amount will be transfered to the address on _receivers with the same index.
      * @notice Currently the function only works with only one ERC20 token
-     * @dev TODO: Change onlyOwner to openzeppelin roles.
-     * @dev TODO: Extract interface to another file
      */
-    function performPayment(address _erc20TokenAddress, 
-                            address[] calldata _receivers, 
-                            uint256[] calldata _ammountsToTransfer) 
-                external onlyOwner {
+    function performPayment(address _erc20TokenAddress,
+        address[] calldata _receivers,
+        uint256[] calldata _amountsToTransfer)
+    external onlyRole(PAYER_ROLE) {
         console.log("Performing a batch payment");
-        require(_ammountsToTransfer.length == _receivers.length, "Both arrays must have the same length");
-        
+        require(_amountsToTransfer.length == _receivers.length, "Both arrays must have the same length");
+
         address currentReceiver;
         uint256 currentAmount;
-        ERC20Basic erc20token = ERC20Basic(_erc20TokenAddress);
+        IERC20Basic erc20token = IERC20Basic(_erc20TokenAddress);
 
         for (uint256 i = 0; i < _receivers.length; i++) {
             currentReceiver = _receivers[i];
             console.log("Paying to: ", currentReceiver);
             require(_receivers[i] != address(0), "ERC20: cannot register a 0 address");
-            
-            currentAmount = _ammountsToTransfer[i];
-            console.log("Amount: ", currentAmount);
 
+            currentAmount = _amountsToTransfer[i];
+            console.log("Amount: ", currentAmount);
             erc20token.transferFrom(msg.sender, currentReceiver, currentAmount);
         }
         console.log("Finished batch payment");
-        emit BatchPaymentFinished(_receivers, _ammountsToTransfer);
+        emit BatchPaymentFinished(_receivers, _amountsToTransfer);
     }
 }
