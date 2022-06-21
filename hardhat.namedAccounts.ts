28f7314e629b094f462be78b1f06a4cd266d885a
djaciel <dj_roses@hotmail.com>
2022-06-21 14:21:07 -0500

    add namedAccounts for telos networks

--- hardhat.namedAccounts.ts
+++ hardhat.namedAccounts.ts
@@ -16,6 +16,8 @@ const networks: HardhatUserConfig['namedAccounts'] = {
     4: 0, // For testnet use first account
     56: 0,
     97: 0, // For testnet use first account
+    40: 0,
+    41: 0, // For testnet use first account
   },
   swapRouter: {
     default: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
@@ -23,6 +25,8 @@ const networks: HardhatUserConfig['namedAccounts'] = {
     4: '0xE592427A0AEce92De3Edee1F18E0157C05861564', // uniswap v3
     56: '0x10ED43C718714eb63d5aA57B78B54704E256024E', // pancakeswap v2
     97: '0x3380aE82e39E42Ca34EbEd69aF67fAa0683Bb5c1', // apeswap v2 or 0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3 https://pancake.kiemtienonline360.com/
+    40: '0xB9239AF0697C8efb42cBA3568424b06753c6da71', // zappy.finance v2
+    41: '0xd03d102C9dfCE013eA4671B5c282D65Cf1eB1DC5', // demo.telos.finance v2
   },
   dai: {
     1: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
