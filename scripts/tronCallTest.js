e9a8c745610873471f3f3e5ca6d09772c78829a7
djaciel <dj_roses@hotmail.com>
2022-08-01 09:28:43 -0500

    add tron readme

--- scripts/tronCallTest.js
+++ scripts/tronCallTest.js
@@ -16,32 +16,31 @@ const payrollAddress = 'TD1o2DV6E5iXu2dPbu1nFPRozbqNEtiBP4';
 const USDT = 'TSsjPxRVQxN5CKk59Gfxyme5vhgp3ymSKq';
 const ADA = 'TTMuLqv8eLSVUbdR8U6rPH99HZKGAfX7J2';
 
+const swaps = [['1006967980', '2200000000000000000000', [USDT, ADA]]];
+
+const payments = [
+  [
+    USDT,
+    ['TBFQ6CDifCA3wMPG2jGDCDpcP8oGvEoiV3', 'TR33WVeGWp5y6Z2xt1F5dgqjBrTzeYqF1n'],
+    ['1534235000000000000000', '2356670000000000000000'],
+  ],
+  [ADA, ['TBFQ6CDifCA3wMPG2jGDCDpcP8oGvEoiV3', 'TR33WVeGWp5y6Z2xt1F5dgqjBrTzeYqF1n'], ['498499000', '498499000']],
+];
+
+const totalAmountToSwap = '2200000000000000000000';
+
+const timestamp = Date.now() + 1000 * 60 * 60;
+const deadline = Math.floor(timestamp / 1000);
+
 const init = async () => {
-  let tronWeb = getTronWeb();
-  let contract = await tronWeb.contract(abi, payrollAddress);
+  const tronWeb = getTronWeb();
+  const contract = await tronWeb.contract(abi, payrollAddress);
   const myAddress = tronWeb.defaultAddress.base58;
   console.log('My address: ', myAddress);
 
   try {
-    let data = await contract.methods
-      .performSwapV2AndPayment(
-        USDT,
-        '2200000000000000000000',
-        1698430773,
-        [['1006967980', '2200000000000000000000', [USDT, ADA]]],
-        [
-          [
-            USDT,
-            ['TBFQ6CDifCA3wMPG2jGDCDpcP8oGvEoiV3', 'TR33WVeGWp5y6Z2xt1F5dgqjBrTzeYqF1n'],
-            ['1534235000000000000000', '2356670000000000000000'],
-          ],
-          [
-            ADA,
-            ['TBFQ6CDifCA3wMPG2jGDCDpcP8oGvEoiV3', 'TR33WVeGWp5y6Z2xt1F5dgqjBrTzeYqF1n'],
-            ['498499000', '498499000'],
-          ],
-        ]
-      )
+    const data = await contract.methods
+      .performSwapV2AndPayment(USDT, totalAmountToSwap, deadline, swaps, payments)
       .send();
 
     console.log(data);
