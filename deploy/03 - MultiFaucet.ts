8800ddd35db07ad6f718e12973f01079eb0a3868
Jaciel Gzz <42042747+djaciel@users.noreply.github.com>
2022-08-10 18:37:48 -0500

    PTD 621 Multi Faucet contract (#50)
    * create multi faucet contract for testnet tokens
    
    * prettier fix
    
    * fix some code smells

---
+++ deploy/03 - MultiFaucet.ts
@@ -0,0 +1,65 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const version = 'v0.1.0';
+const contractName = 'MultiFaucet';
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    const {deployments, getNamedAccounts, network} = hre;
+    if (!network.live) return;
+    if (!network.tags['staging']) return;
+
+    console.log(`Deploying ${contractName} ${version}`);
+
+    const {deploy} = deployments;
+
+    const {deployer} = await getNamedAccounts();
+
+    const deployResult = await deploy(contractName, {
+      contract: contractName,
+      from: deployer,
+      gasLimit: 2000000,
+      log: true,
+    });
+
+    if (deployResult.newlyDeployed && deployResult.transactionHash) {
+      const blocks = 5;
+      console.log(`Waiting ${blocks} blocks before verifying`);
+      await hre.ethers.provider.waitForTransaction(deployResult.transactionHash, blocks);
+      try {
+        console.log(`Startig Verification of MultiFaucet_Implementation ${deployResult.address}`);
+        await hre.run('verify:verify', {
+          address: deployResult.address,
+        });
+      } catch (err: any) {
+        if (err.message.includes('Already Verified')) {
+          return;
+        }
+        throw err;
+      }
+    }
+
+    return true;
+  }
+
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  await main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+};
+
+const id = contractName + version;
+
+export default func;
+func.tags = [contractName, version];
+func.id = id;
---
+++ test/MultiFaucet.test.ts
@@ -0,0 +1,94 @@
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {ethers, network} from 'hardhat';
+import {expect} from 'chai';
+import {Token, MultiFaucet} from '../typechain-types';
+
+let tokenA: Token;
+let tokenB: Token;
+let multiFaucet: MultiFaucet;
+let userA: SignerWithAddress;
+let userB: SignerWithAddress;
+
+describe('Contract: MultiFaucet', () => {
+  beforeEach(async () => {
+    await network.provider.request({
+      method: 'hardhat_reset',
+    });
+
+    [userA, userB] = await ethers.getSigners();
+
+    const TokenInstance = await ethers.getContractFactory('Token');
+    tokenA = (await TokenInstance.deploy('Token_A', 'TKA')) as Token;
+    tokenB = (await TokenInstance.deploy('Token_A', 'TKA')) as Token;
+
+    const MultiFaucetInstance = await ethers.getContractFactory('MultiFaucet');
+    multiFaucet = (await MultiFaucetInstance.deploy()) as MultiFaucet;
+
+    await multiFaucet.addFaucet(tokenA.address, 100);
+    await tokenA.transfer(multiFaucet.address, 100);
+  });
+
+  describe('Faucet', () => {
+    it('should add a faucet', async () => {
+      const faucet = await multiFaucet.faucets(tokenA.address);
+      expect(faucet[0]).to.equal(userA.address);
+      expect(faucet[1]).to.equal(100);
+    });
+
+    it('should not add an existing faucet', async () => {
+      await expect(multiFaucet.addFaucet(tokenA.address, 100)).to.be.revertedWith(
+        'MultiFaucet: This faucet already exist, you can still deposit funds'
+      );
+    });
+
+    it('should update a faucet', async () => {
+      await multiFaucet.updateFaucet(tokenA.address, 200);
+      const faucet = await multiFaucet.faucets(tokenA.address);
+      expect(faucet[0]).to.equal(userA.address);
+      expect(faucet[1]).to.equal(200);
+    });
+
+    it('should not update the faucet if you are not the owner', async () => {
+      await expect(multiFaucet.connect(userB).updateFaucet(tokenA.address, 200)).to.be.revertedWith(
+        'MultiFaucet: You are not the owner of this faucet'
+      );
+    });
+
+    it('should remove a faucet', async () => {
+      let faucetBalance = await tokenA.balanceOf(multiFaucet.address);
+      expect(faucetBalance).to.equal(100);
+      await multiFaucet.removeFaucet(tokenA.address);
+      faucetBalance = await tokenA.balanceOf(multiFaucet.address);
+      expect(faucetBalance).to.equal(0);
+      const faucet = await multiFaucet.faucets(tokenA.address);
+      expect(faucet[0]).to.equal(ethers.constants.AddressZero);
+      expect(faucet[1]).to.equal(0);
+    });
+
+    it('should not remove the faucet if you are not the owner', async () => {
+      await expect(multiFaucet.connect(userB).removeFaucet(tokenA.address)).to.be.revertedWith(
+        'MultiFaucet: You are not the owner of this faucet'
+      );
+    });
+
+    it('should request funds', async () => {
+      const faucet = await multiFaucet.faucets(tokenA.address);
+      await multiFaucet.connect(userB).requestFunds(tokenA.address);
+      const userBalance = await tokenA.balanceOf(userB.address);
+      expect(userBalance).to.equal(faucet[1]);
+    });
+
+    it('should throw error when a faucet does not have enough funds', async () => {
+      await multiFaucet.connect(userB).requestFunds(tokenA.address);
+      await expect(multiFaucet.connect(userB).requestFunds(tokenA.address)).to.be.revertedWith(
+        'MultiFaucet: This faucet does not have enough funds'
+      );
+    });
+
+    it('should throw error when a faucet does not exist', async () => {
+      await expect(multiFaucet.connect(userB).requestFunds(tokenB.address)).to.be.revertedWith(
+        'MultiFaucet: This faucet does not exist'
+      );
+    });
+  });
+});
